b5a61d6b27265bbd49055d5956d11e2c2a108718
Sergio Lillo <sergiolillom@gmail.com>
2026-02-22 13:06:54 +0100

    Houses list and form screens implemented

---
+++ lib/screens/house_form_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:hogwarts_with_flutter/services/house_service.dart';
+import '../models/house.dart';
+
+
+class HouseFormScreen extends StatefulWidget{
+  //Si agregamos, no tenemos casa (vacía); si editamos, tenemos casa a editar
+  final House? house;
+  final VoidCallback onSaved; //Callback para refrescar la lista después de guardar
+
+  const HouseFormScreen({super.key, this.house, required this.onSaved});
+
+  @override
+  State<StatefulWidget> createState() => _HouseFormScreenState();
+}
+
+class _HouseFormScreenState extends State<HouseFormScreen>{
+  final service = HouseService();
+  final nameCtrl = TextEditingController(); //Controlador para el campo name
+  final founderCtrl = TextEditingController(); //Controlador para el campo founder
+
+  //Función que se ejecuta cuando se crea el widget
+  //Si editamos (house !=null), llenamos los campos con los datos que tenemos de la casa
+  @override
+  void initState() {
+    super.initState();
+    if (widget.house != null) {
+      nameCtrl.text = widget.house?.name ?? '';
+      founderCtrl.text = widget.house?.founder ?? '';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isEdit = widget.house != null;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(isEdit ? "Edit house" : "Add new house"),
+        ),
+        body: 
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: nameCtrl,
+                decoration: const InputDecoration(labelText: "House name"),
+              ),
+              TextField(
+                controller: founderCtrl,
+                decoration: const InputDecoration(labelText: "House founder name"),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                child: Text(isEdit ? "Update" : "Save"),
+                onPressed: () async {
+                  if (isEdit) {
+                    await service.updateHouse(widget.house!.id, nameCtrl.text, founderCtrl.text);
+                  } else {
+                    await service.addHouse(nameCtrl.text, founderCtrl.text);
+                  }
+                  widget.onSaved(); //Refrescar la lista
+                  Navigator.pop(context); //Volver a la pantalla anterior
+                },
+              )
+            ],
+          ),
+        ),
+    );
+  }
+
+}
--- lib/screens/houses_list_screen.dart
+++ lib/screens/houses_list_screen.dart
@@ -1,17 +1,72 @@
 import 'package:flutter/material.dart';
+import '../models/house.dart';
+import '../services/house_service.dart';
+import '../screens/house_form_screen.dart';
 
-class HousesListScreen extends StatelessWidget {
+class HousesListScreen extends StatefulWidget {
   const HousesListScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HousesListScreenState();
+}
+
+
+class _HousesListScreenState extends State<HousesListScreen> {
+
+  final service = HouseService();
+  late Future<List<House>> futureHouses;
+  
+  //Método inicial para cargar de primeras el contenido
+  @override
+  void initState() {
+    super.initState();
+    futureHouses = service.getHouses();
+  }
+
+    //Método para recargar casas cuando hay un cambio de estado en la lista
+  void _refresh() {
+    setState(() {
+      futureHouses = service.getHouses();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
          appBar: AppBar(
-          title: const Text("Houses' list"),
+          title: const Text("Houses list"),
         ),
-        body: const Center(
-          child: Text("Here we will show the houses..."),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => HouseFormScreen(onSaved: _refresh,))),
+          child: const Icon(Icons.add),
+        ),
+        body: FutureBuilder(
+          future: futureHouses, 
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final houses = snapshot.data!; //List(houses)
+            return ListView.builder(
+                itemCount: houses.length,
+                itemBuilder: (context, index) {
+                  final house = houses[index];
+                  return ListTile(
+                    title: Text('House: ${house.name}'),
+                    subtitle: Text('Founder: ${house.founder}'),
+                    trailing: IconButton(
+                      onPressed: () async {
+                        await service.deleteHouse(house.id);
+                        _refresh();
+                        },
+                      icon: const Icon(Icons.delete)),
+                      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => HouseFormScreen(house: house, onSaved: _refresh)))
+                  );
+                }
+              );
+          }
         )
     );
   }
+  
 }
