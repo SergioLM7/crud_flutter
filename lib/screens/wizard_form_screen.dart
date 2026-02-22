cdd03c20230a323685700daae77fd20ad0e0bf2f
Sergio Lillo <sergiolillom@gmail.com>
2026-02-22 18:53:07 +0100

    Wizards list and form screen implemented

---
+++ lib/screens/wizard_form_screen.dart
@@ -0,0 +1,164 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import '../models/wizard.dart';
+import '../models/wand.dart';
+import '../models/house.dart'; 
+import '../services/wizard_service.dart';
+import '../services/wand_service.dart';
+import '../services/house_service.dart';
+
+class WizardFormScreen extends StatefulWidget{
+  //Si agregamos, no tenemos mago (vacío); si editamos, tenemos mago a editar
+  final Wizard? wizard;
+  final VoidCallback onSaved; //Callback para refrescar la lista después de guardar
+
+  const WizardFormScreen({super.key, this.wizard, required this.onSaved});
+
+  @override
+  State<StatefulWidget> createState() => _WizardFormScreenState();
+}
+
+class _WizardFormScreenState extends State<WizardFormScreen>{
+  final service = WizardService();
+  final wandService = WandService();
+  final houseService = HouseService();
+  final nameCtrl = TextEditingController(); //Controlador para el campo name
+  final ageCtrl = TextEditingController(); //Controlador para el campo age
+  final houseNameCtrl = TextEditingController(); //Controlador para el campo house name
+  final wandCoreCtrl = TextEditingController(); //Controlador para el campo wand core
+  final wandWoodCtrl = TextEditingController(); //Controlador para el campo wand wood
+
+  List<House> housesList = [];
+  List<Wand> wandsList = [];
+
+  //Variables para guardar la selección actual de los dropdowns
+  String? selectedHouseId;
+  String? selectedWandId;
+
+  //Función que se ejecuta cuando se crea el widget
+  //Si editamos (wand !=null), llenamos los campos con los datos que tenemos de la varita
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    final houses = await houseService.getHouses();
+    final wands = await wandService.getWands();
+
+    if(widget.wizard !=null) {
+      final w = widget.wizard!;
+      nameCtrl.text = w.name;
+      ageCtrl.text = w.age.toString();
+      selectedHouseId = w.houseId;
+      selectedWandId = w.wandId;
+    }
+
+    setState(() {
+      housesList = houses;
+      wandsList = wands;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    final isEdit = widget.wizard != null;
+
+    // Tema local para dar formato tipo TextField a los DropdownMenu
+    final dropdownTheme = Theme.of(context).copyWith(
+      inputDecorationTheme: const InputDecorationTheme(
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+      ),
+    );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(isEdit ? "Edit wizard" : "Add new wizard"),
+        ),
+        body: 
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: nameCtrl,
+                decoration: const InputDecoration(labelText: "Name"),
+              ),
+              TextField(
+                controller: ageCtrl,
+                decoration: const InputDecoration(labelText: "Age (years)"),
+                keyboardType: Platform.isIOS ? const TextInputType.numberWithOptions(signed: false, decimal: false) : TextInputType.number,
+              ),
+              const SizedBox(height: 24),
+              Theme(
+                data: dropdownTheme,
+                child: DropdownMenu<String>(
+                  initialSelection: selectedHouseId,
+                  label: const Text("House", style: TextStyle(fontWeight: FontWeight.bold)),
+                  menuStyle: MenuStyle(
+                    backgroundColor: WidgetStateProperty.all(Colors.white),
+                    padding: WidgetStateProperty.all(const EdgeInsets.all(8)),
+                  ),
+
+                  dropdownMenuEntries: housesList
+                    .map((h) =>
+                      DropdownMenuEntry<String>(value: h.id, label: h.name))
+                      .toList(),
+
+                      onSelected: (value) =>
+                          setState(() => selectedHouseId = value),
+                  ),
+              ),
+              const SizedBox(height: 16),
+              // VARITA — igual estilo
+              Theme(
+                data: dropdownTheme,
+                child: DropdownMenu<String>(
+                  initialSelection: selectedWandId,
+                  label: const Text("Wand ", style: TextStyle(fontWeight: FontWeight.bold)),
+                  menuStyle: MenuStyle(
+                    backgroundColor: WidgetStateProperty.all(Colors.white),
+                    padding: WidgetStateProperty.all(const EdgeInsets.all(8)),
+                  ),
+
+                  dropdownMenuEntries: wandsList
+                    .map((w) => DropdownMenuEntry<String>(
+                          value: w.id,
+                          label: "${w.wood} - ${w.core}",
+                        ),
+                    ).toList(),
+
+                  onSelected: (value) =>
+                      setState(() => selectedWandId = value),
+                  ),
+              ),
+
+              const SizedBox(height: 40), // deja hueco para el botón fijo
+              ElevatedButton(
+                child: Text(isEdit ? "Update" : "Save"),
+                onPressed: () async {
+                  final age = int.tryParse(ageCtrl.text) ?? 0;
+
+                  if (isEdit) {
+                    await service.updateWizard(widget.wizard!.id, nameCtrl.text, age, selectedHouseId ?? '', selectedWandId ?? '');
+                  } else {
+                    await service.addWizard(nameCtrl.text, age, selectedHouseId ?? '', selectedWandId ?? '');
+                  }
+                  widget.onSaved(); //Refrescar la lista
+                  Navigator.pop(context); //Volver a la pantalla anterior
+                },
+
+              )
+            ],
+          ),
+        ),
+    );
+  }
+}
--- lib/screens/wizards_list_screen.dart
+++ lib/screens/wizards_list_screen.dart
@@ -1,17 +1,142 @@
 import 'package:flutter/material.dart';
+import '../services/wizard_service.dart';
+import '../models/wizard.dart';
+import '../screens/wizard_form_screen.dart';
 
-class WizardsListScreen extends StatelessWidget {
+class WizardsListScreen extends StatefulWidget {
   const WizardsListScreen({super.key});
+  
+  @override
+  State<StatefulWidget> createState() => _WizardsListScreenState();
+}
+
+class _WizardsListScreenState extends State<WizardsListScreen> {
+  final service = WizardService();
+
+  late Future<List<Wizard>> futureWizards;
+
+  @override
+  void initState() {
+    super.initState();
+    futureWizards = service.getWizards();
+  }
+
+  void _refresh() {
+    setState(() {
+      futureWizards = service.getWizards();
+    });
+  }
+
+  /// Diálogo de confirmación antes de eliminar
+  Future<bool> _confirmDelete(BuildContext context) async {
+    return await showDialog<bool>(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: const Text("Do you want to delete the wizard?"),
+            content: const Text("This action can not be undone."),
+            actions: [
+              TextButton(
+                child: const Text("Cancel"),
+                onPressed: () => Navigator.pop(context, false),
+              ),
+              TextButton(
+                child: const Text(
+                  "Delete",
+                  style: TextStyle(color: Colors.red),
+                ),
+                onPressed: () => Navigator.pop(context, true),
+              ),
+            ],
+          ),
+        ) ?? false;
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         appBar: AppBar(
-          title: const Text("Wizards' list"),
+        appBar: AppBar(
+          title: const Text("Wizards list"),
         ),
-        body: const Center(
-          child: Text("Here we will show the wizards..."),
-        )
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => WizardFormScreen(onSaved: _refresh))), 
+        ),
+        body: 
+          FutureBuilder(
+          future: futureWizards, 
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            
+            final wizards = snapshot.data!; //List(wizards)
+
+            if(wizards.isEmpty) {
+              return const Center(
+                child: Text('No wizards found\n Pulse + button to add one.', 
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20),
+                  ),
+              );
+            }
+
+            return ListView.builder(
+                itemCount: wizards.length,
+                itemBuilder: (context, index) {
+                  final w = wizards[index];
+                  final house = w.houseName ?? 'TBC';
+                  final wand = (w.wandCore != null && w.wandWood != null) ? '${w.wandWood} - ${w.wandCore}' : 'TBC';
+
+                  return Card(
+                    margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    child: ListTile(
+                      leading: Icon(Icons.rowing),
+                      title: Text('Wizard: ${w.name}', style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),), 
+                      subtitle: Text('Age: ${w.age}\nHouse: $house\nWand: $wand'),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          // --- ICONO EDITAR ---
+                          IconButton(
+                            icon: const Icon(Icons.edit, color: Colors.blueAccent),
+                            tooltip: "Update wizard",
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => WizardFormScreen(
+                                    wizard: w,
+                                    onSaved: _refresh,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+
+                          // --- ICONO ELIMINAR ---
+                          IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.red),
+                            tooltip: "Delete wizard",
+                            onPressed: () async {
+                              final confirm = await _confirmDelete(context);
+                              if (confirm) {
+                                await service.deleteWizard(w.id);
+                                _refresh();
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              );
+            }
+          )
     );
   }
 }
+
