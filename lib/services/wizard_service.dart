49de2ef71e5a169dd48cf9fd85828d0920451afe
Sergio Lillo <sergiolillom@gmail.com>
2026-02-22 18:30:19 +0100

    Fix with house and wand tables name in Wizard Service

--- lib/services/wizard_service.dart
+++ lib/services/wizard_service.dart
@@ -12,11 +12,11 @@ class WizardService {
   }
 
   Future<void> addWizard(String name, int age, String? houseId, String? wandId) async {
-    await _supabaseDB.from('wizard').insert({'name': name, 'age': age, 'houseId': houseId, 'wandId': wandId});
+    await _supabaseDB.from('wizard').insert({'name': name, 'age': age, 'house_id': houseId, 'wand_id': wandId});
   }
 
   Future<void> updateWizard(String id, String name, int age, String? houseId, String? wandId) async {
-    await _supabaseDB.from('wizard').update({'name': name, 'age': age, 'houseId': houseId, 'wandId': wandId}).eq('id', id);
+    await _supabaseDB.from('wizard').update({'name': name, 'age': age, 'house_id': houseId, 'wand_id': wandId}).eq('id', id);
   }
 
   Future<void> deleteWizard(String id) async {
